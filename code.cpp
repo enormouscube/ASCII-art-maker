7c656d3f4d149a4a322e95e5f14092835ae33ca5
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-06-03 04:27:22 +0300

    refactoring

--- code.cpp
+++ code.cpp
@@ -1,63 +1,69 @@
 ﻿#include "lodepng/lodepng.h"
 
+#include <exception>
+#include <format>
+#include <sstream>
 #include <vector>
 #include <iostream>
 #include <map>
 #include <fstream>
 #include <expected>
 
-//decoding function
-inline auto DecodeOneStep(auto &image, std::string_view filename) -> std::expected<uint32_t, uint32_t> {
+inline constexpr auto DecodeOneStep(auto &image, std::string_view filename) -> std::expected<uint32_t, uint32_t> {
     uint32_t width, height;
-    auto error = lodepng::decode(image, width, height, filename.data());
-    if (error) std::cout << "I CAN'T WORK!!\n";
+    if (auto error = lodepng::decode(image, width, height, filename.data()); error) {
+        return std::unexpected(error);
+    }
+
     return width;
 }
 
-
-//turning image to grayscale function
-void ToGray(std::vector<unsigned char> &image, std::vector<unsigned> &gray_image, int n) {
-    for (int i = 0; i < n; i += 4) gray_image.push_back((image[i] + image[i + 1] + image[i + 2]) / 3);
+inline constexpr auto ToGray(auto &image, auto &gray_image, auto n) {
+    for (auto i = 0; i < n; i += 4) {
+        gray_image.emplace_back((image[i] + image[i + 1] + image[i + 2]) / 3);
+    }
 }
 
 int main() {
-    std::vector<unsigned char> image;
-    unsigned w = 0;
-    std::vector<unsigned> gray_image;
+    std::vector<uint8_t> image;
+    uint32_t w = 0;
+    std::vector<uint32_t> gray_image;
 
-    //map to encode image to ASCII
-    std::map<unsigned, char> ENCODE_MAP = { {(unsigned)0, '@'},
-{(unsigned)1, '&'},
-{(unsigned)2, '#'},
-{(unsigned)3, 'G'},
-{(unsigned)4, 'H'},
-{(unsigned)5, 'P'},
-{(unsigned)6, 'I'},
-{(unsigned)7, 'L'},
-{(unsigned)8, '|'},
-{(unsigned)9, '('},
-{(unsigned)10, '['},
-{(unsigned)11, ':'},
-{(unsigned)12, '*'},
-{(unsigned)13, '"'},
-{(unsigned)14, '^'},
-{(unsigned)15, '.'},
+    constexpr char const ENCODE_MAP[] = {
+        '@',
+        '&',
+        '#',
+        'G',
+        'H',
+        'P',
+        'I',
+        'L',
+        '|',
+        '(',
+         '[',
+         ':',
+         '*',
+         '"',
+         '^',
+         '.',
     };
 
-    w = DecodeOneStep(image, "your_image.png");
+    w = DecodeOneStep(image, "your_image.png").transform_error([](auto error) -> uint32_t {
+        std::cout << "decode error: " << error << std::endl;
+        std::terminate();
+    }).value();
+
     ToGray(image, gray_image, image.size());
     
-    //turning image into string to write it into file
-    std::string finalstr = "";
-    for (int i = 0; i < gray_image.size(); ++i) {
-        finalstr += ENCODE_MAP[gray_image[i] / 16];
-        if ((i + 1) % w == 0) finalstr += "\n";
+    std::stringstream finalstr;
+    for (auto i = 0; i < gray_image.size(); ++i) {
+        finalstr << ENCODE_MAP[gray_image[i] / 16];
+
+        if ((i + 1) % w == 0) {
+            finalstr << std::endl;
+        }
     }
 
-    //writing string into file
-    std::ofstream file;
-    file.open("result.txt");
-    if (file.is_open()) file << finalstr;
-    file.close();
-    return 0;
+    std::ofstream file("result.txt");
+    file << finalstr.str();
 }
